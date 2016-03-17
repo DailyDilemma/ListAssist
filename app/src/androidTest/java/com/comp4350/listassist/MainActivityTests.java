cdc18eeb5b2be75fd778d3ecef3baedba4d7ca31
matt <evolmatt@gmail.com>
2016-03-16 23:53:03 -0500

    Adds some simple tests for main and view activities.

---
+++ app/src/androidTest/java/com/comp4350/listassist/MainActivityTests.java
@@ -0,0 +1,22 @@
+package com.comp4350.listassist;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTests() {
+        super(MainActivity.class);
+    }
+
+    public void testActivityExists() {
+        MainActivity actv = getActivity();
+        assertNotNull(actv);
+    }
+
+    public void testEnterNewListName() {
+        MainActivity actv = getActivity();
+
+        //final EditText listNameText = (EditText) actv.
+    }
+}
---
+++ app/src/androidTest/java/com/comp4350/listassist/ViewActivityTests.java
@@ -0,0 +1,22 @@
+package com.comp4350.listassist;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+public class ViewActivityTests extends ActivityInstrumentationTestCase2<ViewActivity> {
+
+    public ViewActivityTests() {
+        super(ViewActivity.class);
+    }
+
+    public void testActivityExists() {
+        ViewActivity actv = getActivity();
+        assertNotNull(actv);
+    }
+
+    public void testEnterNewListItemName() {
+        ViewActivity actv = getActivity();
+
+        //final EditText listItemNameText = (EditText) actv.
+    }
+}
