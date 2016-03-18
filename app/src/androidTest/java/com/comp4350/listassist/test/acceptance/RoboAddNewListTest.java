96dcdfb114df627a973e55874ec13ec7c7f8d3a1
Matt <evolmatt@gmail.com>
2016-03-18 15:50:34 -0500

    ?

--- app/src/androidTest/java/com/comp4350/listassist/test/acceptance/RoboAddNewListTest.java
+++ app/src/androidTest/java/com/comp4350/listassist/test/acceptance/RoboAddNewListTest.java
@@ -1,6 +1,7 @@
 package com.comp4350.listassist.test.acceptance;
 
 import com.comp4350.listassist.presentation.MainActivity;
+import com.comp4350.listassist.presentation.ViewActivity;
 import com.robotium.solo.*;
 import android.test.ActivityInstrumentationTestCase2;
 
@@ -25,11 +26,17 @@ public class RoboAddNewListTest extends ActivityInstrumentationTestCase2<MainAct
   	}
   
 	public void testRun() {
-        //Wait for activity: 'com.comp4350.listassist.MainActivity'
-		assertTrue(solo.waitForActivity(com.comp4350.listassist.presentation.MainActivity.class, 2000));
+		//Wait for activity: 'com.comp4350.listassist.presentation.MainActivity'
+		solo.waitForActivity(MainActivity.class, 2000);
 
+		//Click on Add button
+        solo.clickOnImageButton(1);
 
-        //Click on Confirm
-		solo.clickOnView(solo.getView(android.R.id.button1));
+        solo.clickOnEditText(0);
+        solo.enterText(0, "New Robo List");
+
+        solo.clickOnButton(1);
+        
+        solo.goBack();
 	}
 }
