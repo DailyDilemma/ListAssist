98a85f3893ac7667b339cdc5448202c9d3957887
Matt <evolmatt@gmail.com>
2016-03-30 11:43:22 -0500

    Adds a simple junit test

--- app/src/androidTest/java/com/comp4350/listassist/test/presentation/MainActivityTest.java
+++ app/src/androidTest/java/com/comp4350/listassist/test/presentation/MainActivityTest.java
@@ -1,6 +1,8 @@
 package com.comp4350.listassist.test.presentation;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+
 import com.comp4350.listassist.presentation.MainActivity;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
@@ -9,6 +11,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         super(MainActivity.class);
     }
 
+    @UiThreadTest
     public void testActivityExists() {
         MainActivity actv = getActivity();
         assertNotNull(actv);
--- app/src/androidTest/java/com/comp4350/listassist/test/presentation/ViewActivityTest.java
+++ app/src/androidTest/java/com/comp4350/listassist/test/presentation/ViewActivityTest.java
@@ -1,6 +1,8 @@
 package com.comp4350.listassist.test.presentation;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+
 import com.comp4350.listassist.presentation.ViewActivity;
 
 public class ViewActivityTest extends ActivityInstrumentationTestCase2<ViewActivity> {
@@ -9,6 +11,7 @@ public class ViewActivityTest extends ActivityInstrumentationTestCase2<ViewActiv
         super(ViewActivity.class);
     }
 
+    @UiThreadTest
     public void testActivityExists() {
         ViewActivity actv = getActivity();
         assertNotNull(actv);
